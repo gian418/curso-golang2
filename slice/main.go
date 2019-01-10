ac9487510ed6588a60ca3e051b1605cb64b47ad5
gian418 <gian418@gmail.com>
2019-01-10 15:47:33 -0200

    Aula 22 - Slices parte 2

--- slice/main.go
+++ slice/main.go
@@ -3,27 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	var nums []int
-	fmt.Println(nums, len(nums), cap(nums))
-	nums = make([]int, 5)
-	fmt.Println(nums, len(nums), cap(nums))
+	//var nums []int
+	//fmt.Println(nums, len(nums), cap(nums))
+	//nums = make([]int, 5)
+	//fmt.Println(nums, len(nums), cap(nums))
 
 	capitais := []string{"Lisboa"}
-	fmt.Println(capitais, len(capitais), cap(capitais))
+	//fmt.Println(capitais, len(capitais), cap(capitais))
 	capitais = append(capitais, "Brasília")
-	fmt.Println(capitais, len(capitais), cap(capitais))
+	//fmt.Println(capitais, len(capitais), cap(capitais))
 
-	cidades := make([]string, 4)
+	cidades := make([]string, 5)
 	cidades[0] = "Nova York"
-	cidades[1] = "Londes"
-	cidades[2] = "Tokio"
-	cidades[3] = "Singapura"
-	fmt.Println(cidades, len(cidades), cap(cidades))
+	cidades[1] = "Londres"
+	cidades[2] = "Madeira"
+	cidades[3] = "Tokio"
+	cidades[4] = "Singapura"
+	//fmt.Println(cidades, len(cidades), cap(cidades))
 
 	capitais[1] = "Brasilia"
-	fmt.Println(capitais, len(capitais), cap(capitais))
+	//fmt.Println(capitais, len(capitais), cap(capitais))
 
 	for indice, cidade := range cidades {
-		fmt.Printf("Cidade[%d] = %s\n\r", indice, cidade)
+		fmt.Printf("Cidade[%d] = %s\n", indice, cidade)
 	}
+
+	//Primeiro item começa com indice 0
+	//Segundo item começa com o indice 1
+	capitaisAsia := cidades[3:5]
+	fmt.Println(capitaisAsia, len(capitaisAsia), cap(capitaisAsia))
+
+	temp1 := cidades[:2]
+	fmt.Println(temp1, len(temp1), cap(temp1))
+
+	temp2 := cidades[len(cidades)-2:]
+	fmt.Println(temp2)
+
+	indiceDoItemARetirar := 2
+	temp := cidades[:indiceDoItemARetirar]
+	temp = append(temp, cidades[indiceDoItemARetirar+1:]...)
+	cidades = temp
+	fmt.Println(cidades)
 }
